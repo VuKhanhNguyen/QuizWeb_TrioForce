195d16c2a849eff2a4cf17fb8774798f492f9374
NguyenVuKhanh <nguyenvukhanh392@gmail.com>
2025-11-19 02:52:13 +0700

    sửa hiển thị username khi register thành công

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -130,7 +130,7 @@ namespace QuizWeb_TrioForce.Areas.Identity.Pages.Account
                 var user = CreateUser();
                 user.FullName = Input.FullName;
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
